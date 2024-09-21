7f4586c3bb54d5edc0a733854729deedba81dc59
Andy Leverenz <andy@justalever.com>
2024-09-21 13:39:50 -0500

    Simplify stuff

--- presets/retriever/dropdown.js
+++ presets/retriever/dropdown.js
@@ -1,7 +1,19 @@
 const plugin = require("tailwindcss/plugin")
-const { hexToRgba } = require("../../utils")
 
 module.exports = plugin(function ({ addComponents, theme }) {
+  function hexToRgba(hex, opacity) {
+    // Remove the hash at the start if it's there
+    hex = hex.replace(/^#/, "")
+
+    // Parse the r, g, b values
+    let r = parseInt(hex.substring(0, 2), 16)
+    let g = parseInt(hex.substring(2, 4), 16)
+    let b = parseInt(hex.substring(4, 6), 16)
+
+    // Return the rgba color
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`
+  }
+
   const dropdown = {
     ".dropdown-menu": {
       position: theme("position.absolute"),
